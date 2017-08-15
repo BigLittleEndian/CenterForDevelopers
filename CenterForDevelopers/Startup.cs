62a5546e7dd3282cb4197ab7f57bfb0dd8d04a5b
Vladimir Kojic <vk@BigLittleEndian.com>
2017-08-15 12:31:36 -0400

    Switched to NetCore 2.0

--- CenterForDevelopers/Startup.cs
+++ CenterForDevelopers/Startup.cs
@@ -30,9 +30,7 @@ namespace CenterForDevelopers
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            //services.AddMvc();
-            services.AddMvc().AddRazorOptions(options =>
-                    options.ParseOptions = new CSharpParseOptions(LanguageVersion.CSharp7));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
